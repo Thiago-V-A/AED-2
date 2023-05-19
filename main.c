ed6026db8ffb2ee8d8b6112a969407a0474637b8
Thiago-V-A <118688370+Thiago-V-A@users.noreply.github.com>
2023-05-18 22:08:03 -0300

    Correção, função AVL funcional

--- main.c
+++ main.c
@@ -3,65 +3,99 @@
 #include <string.h>
 #include "arvore.h"
 
-int main(){
-	char nome[100];
-	int codigo, op, estoque;
-	float preco;
-    noarv* arvore = NULL;
-    do{  
-        menu();
-        scanf("%d",&op);
-        switch (op) {
-        case 1:
-            printf("Insira o codigo do produto:\n");
-            scanf("%d", &codigo);
-            printf("Insira a quantidade do produto a ser adicionada:\n");
-            scanf("%d", &estoque);
-            getchar();
-            printf("Insira o nome do produto:\n");
-			fgets(nome, 100, stdin);
-            printf("Insira o preco do produto a ser cadastrado:\n");
-            scanf("%f", &preco);
-            
-            produto novoProduto;
-            novoProduto.codigo = codigo;
-            strcpy(novoProduto.nome, nome);
-            novoProduto.preco = preco;
-            novoProduto.estoque = estoque;
-            
-            arvore = inserirProduto(arvore, novoProduto);
-            
-            break;
-
-        case 2: 
-            break;
-            
-        case 3: 
-            break;
-
-		case 4:
-			printf("Aqui estao os produtos cadastrados:\n");
-			printf("--------------------\n");
-			exibirProdutos(arvore);
-			break;
-
-        case 5:
-            arvore = limparArvore(arvore);
-            printf("A arvore foi reinicializada");
-            break;
-		
-        case 7:
-            arvore = limparArvore(arvore);
-            printf("\nObrigado por usar nosso programa\n");
-            break;
-            
-        default:
-            printf("\nVoce digitou uma opcao invalida, por favor, escolha uma das opcoes:\n");
-            break;  
-        }
-        
-        
-    }while (op!=7);
-    return 0;
+void menu(){ 
+      printf("1 - Cadastrar novo produto\n");
+      printf("2 - Remove produto\n");
+      printf("3 - Buscar produto na arvore\n");
+      printf("4 - Mostrar todos os produtos\n");
+      printf("7 - Sair\n");
+      printf("Entre com a opcao desejada: \n");
 }
 
+Arvore* criarNo(produto Produto){
+    Arvore* novoNo = (Arvore*)malloc(sizeof(Arvore));
+    novoNo->p = Produto;
+    novoNo->esquerda = NULL;
+    novoNo->direita = NULL;
+    return novoNo;
+}
+
+Arvore* inserirProduto(Arvore* raiz, produto Produto){
+    if (!raiz)
+        return criarNo(Produto);
+
+    else{
+		if (Produto.codigo < raiz->p.codigo)
+			raiz->esquerda = inserirProduto(raiz->esquerda, Produto);
+		
+		else if (Produto.codigo > raiz->p.codigo)
+			raiz->direita = inserirProduto(raiz->direita, Produto);
+	
+	}
+	raiz = fbArvore(raiz);
+    return raiz;
+	
+}
+
+void exibirProdutos(Arvore* raiz){
+    if (raiz != NULL) {
+        
+        printf("Codigo: %d\n", raiz->p.codigo);
+        printf("Nome: %s", raiz->p.nome);
+        printf("Preco: %.2f\n", raiz->p.preco);
+        printf("Quantidade: %d\n", raiz->p.estoque);
+        printf("--------------------\n");
+        exibirProdutos(raiz->esquerda);
+        exibirProdutos(raiz->direita);
+    }
+}
+
+int altura(Arvore *arvore){ 
+    if(!arvore)
+        return -1;
+        
+    int altura_esq = altura(arvore->esquerda), altura_dir = altura(arvore->direita);
+    if(altura_esq > altura_dir)
+        return altura_esq + 1;
+    else
+        return altura_dir + 1;
+}
+
+int fatorB(Arvore *arvore){
+    return altura(arvore->direita) - altura(arvore->esquerda);
+}
+
+Arvore* rotacao_E(Arvore *arvore){
+    Arvore *tmp = arvore->direita;
+    arvore->direita = tmp->esquerda;
+    tmp->esquerda = arvore;
+    return tmp;
+}
+
+Arvore* rotacao_D(Arvore *arvore){
+    Arvore *tmp = arvore->esquerda;
+    arvore->esquerda = tmp->direita;
+    tmp->direita = arvore;
+    return tmp;
+}
+
+Arvore* fbArvore(Arvore *arvore){
+
+    int fb = fatorB(arvore); 
+    int fbAux;
+
+    if(fb<-1){
+		fbAux = fatorB(arvore->esquerda);
+        if(fbAux > 0)
+            arvore->esquerda =  rotacao_E(arvore->esquerda);
+        arvore = rotacao_D(arvore);
+        
+    }else if (fb > 1){
+		fbAux = fatorB(arvore->direita);
+        if(fbAux < 0)
+            arvore->direita = rotacao_D(arvore->direita);
+        arvore = rotacao_E(arvore);
+    }
+    
+    return arvore;
+}
